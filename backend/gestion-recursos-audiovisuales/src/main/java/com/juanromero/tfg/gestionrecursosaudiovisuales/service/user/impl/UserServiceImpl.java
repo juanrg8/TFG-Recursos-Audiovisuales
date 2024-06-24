5656545aa855b25c8745cd5f99cd0e13df46c815
Juan Romero González <72883340+juanrg8@users.noreply.github.com>
2024-06-24 07:57:25 +0200

    Error en la subida del método faltante changePassword

--- backend/gestion-recursos-audiovisuales/src/main/java/com/juanromero/tfg/gestionrecursosaudiovisuales/service/user/impl/UserServiceImpl.java
+++ backend/gestion-recursos-audiovisuales/src/main/java/com/juanromero/tfg/gestionrecursosaudiovisuales/service/user/impl/UserServiceImpl.java
@@ -1,23 +1,31 @@
 package com.juanromero.tfg.gestionrecursosaudiovisuales.service.user.impl;
 
-import java.util.ArrayList;    
+import java.util.ArrayList; 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.juanromero.tfg.gestionrecursosaudiovisuales.dto.user.ChangePasswordRequest;
 import com.juanromero.tfg.gestionrecursosaudiovisuales.entity.user.Rol;
 import com.juanromero.tfg.gestionrecursosaudiovisuales.entity.user.User;
 import com.juanromero.tfg.gestionrecursosaudiovisuales.repository.user.UserRepository;
 import com.juanromero.tfg.gestionrecursosaudiovisuales.service.user.UserService;
+
+
+
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public String addUser(User user) {
@@ -89,6 +97,32 @@ public class UserServiceImpl implements UserService {
 			return usuarioOpt.get();
 		}
 		return null;
+	}
+	
+	@Override
+	@Transactional
+	public String changePassword(ChangePasswordRequest request) {
+	    // Buscar el usuario por nombre de usuario
+	    Optional<User> optionalUser = userRepository.findByUsername(request.getUsername());
+	    if (optionalUser.isPresent()) {
+	        User user = optionalUser.get();
+	        
+	        // Verificar que la contraseña actual sea correcta antes de cambiarla
+	        if (passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+	            // Codificar la nueva contraseña
+	            String encodedPassword = passwordEncoder.encode(request.getNewPassword());
+	            user.setPassword(encodedPassword);
+	            
+	            // Guardar el usuario actualizado
+	            userRepository.save(user);
+
+	            return "Contraseña cambiada con éxito.";
+	        } else {
+	            return "Error: La contraseña actual no es correcta";
+	        }
+	    } else {
+	        return "Error: El usuario no existe";
+	    }
 	}
 	
 }
